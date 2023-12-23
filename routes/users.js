7145a8fe8a60526059f1c3e583d0a3558e0073a5
sia162 <nowitissiya@gmail.com>
2023-12-23 18:23:12 +0530

    initialied project and created user schema

---
+++ routes/users.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/pinterestdatabase");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [],
+  dp: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
