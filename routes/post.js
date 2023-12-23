4f68b7dc96c3bbc237acdf3e6ec5d14a9553847e
sia162 <nowitissiya@gmail.com>
2023-12-23 18:28:47 +0530

    created post schema

---
+++ routes/post.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  postText: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Array,
+    default: [],
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
